73c587d87e2ba3007998f0dd617e825f3df1bcf9
Ivan Escribano <ivanescribano1998@gmail.com>
2022-05-12 17:36:50 +0200

    get posts from api and getUSername of the post

--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,40 @@
 const urlPosts = "http://localhost:3000/posts";
+window.onload = () => {
+  getPosts();
+};
 
-const populateBlog = () => {
-    fetch(urlPosts)
-        .then((response) => response.json())
-        .then((data) => {
-            data.map((post) => {
-                let img = createNode('img');
-                img.classList.add("post__image");
-
-                //Titulo//cargar imagen
-                let h2 = createNode('h2');
-                h2.classList.add("post__info-title");
-                h2.textContent = post.title;
-                //Username
-                let p = createNode('p');
-                p.textContent = searchUsername(post.userId);
-                console.log(post.userId);
-
-                console.log('Title: ' + post.title);
-                console.log('Username: ' + searchUsername(post.userId));
-
-            });
-        });
+//!GET DATA
+const getPosts = () => {
+  fetch(urlPosts)
+    .then((response) => response.json())
+    .then((data) => {
+      for (const post of data) {
+        addPosts(post);
+      }
+    });
+};
+//!ADD POSTS HTML
+async function addPosts(post) {
+  //Article
+  const article = document.createElement("article");
+  article.classList.add("post");
+  //Img elment
+  const img = document.createElement("img");
+  img.classList.add("post__image");
+  //create title
+  const h2 = document.createElement("h2");
+  h2.classList.add("post__info-title");
+  h2.textContent = post.title;
+  //ADD USER
+  const username = await getUsername(post.userId);
+  console.log(username);
 }
 
-let searchUsername = (userId) => {
-    let username = "";
-    const urlUsers = `http://localhost:3000/users/${userId}`;
-    fetch(urlUsers)
-        .then((response) => response.json())
-        .then((data) => {
-            // console.log(data.username);
-            username = data.username;
-        });
-        return username;
+//!GET USERNAME
+async function getUsername(userId) {
+  //   let username = "";
+  const userURL = `http://localhost:3000/users/${userId}`;
+  const response = await fetch(userURL);
+  const user = await response.json();
+  return user.username;
 }
-
-//searchUsername(3);
-function createNode(element) {
-    return document.createElement(element);
-}
-
-populateBlog();
